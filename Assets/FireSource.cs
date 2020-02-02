75a258d0892cf89054b493777e816766da6552b3
Dudu_00101010 <ducharme.seb@gmail.com>
2020-02-02 15:46:15 -0500

    FIre extinguisher

--- Assets/FireSource.cs
+++ Assets/FireSource.cs
@@ -7,7 +7,7 @@ public class FireSource : MonoBehaviour
 
     void OnParticleCollision(GameObject collision) {
         if (collision.tag == "Water") {
-            gameObject.GetComponentInParent<Fire>()._hitPoints -= 0.025f;
+            gameObject.GetComponentInParent<Fire>()._hitPoints -= 0.035f;
         }
         if (gameObject.GetComponentInParent<Fire>()._hitPoints <= 0.0f){
             Destroy(gameObject);
