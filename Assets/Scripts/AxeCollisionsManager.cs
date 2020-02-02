93a8803ad6e293ca34f273e6ae9f508f5c2cb9fd
MSI\Ludovic <ludovicaubut@hotmail.com>
2020-02-01 23:05:48 -0500

    Les haches font maintenant des dégats aux murs

---
+++ Assets/Scripts/AxeCollisionsManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeCollisionsManager : MonoBehaviour {
+
+	public float axeDamage;
+
+	void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.CompareTag("Wall")) {
+			col.gameObject.GetComponent<FlammableObject>()._currentHitPoints -= axeDamage;
+		}
+	}
+}
