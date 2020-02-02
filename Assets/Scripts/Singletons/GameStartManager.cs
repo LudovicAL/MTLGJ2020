1802ab17d444f54e88e636a7c9f00866d8633f4e
MSI\Ludovic <ludovicaubut@hotmail.com>
2020-02-02 16:26:40 -0500

    Ajout du GameStartManager

---
+++ Assets/Scripts/Singletons/GameStartManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStartManager : MonoBehaviour {
+
+	public List<GameObject> availableHouses;
+	public static GameStartManager Instance { get; private set;}
+
+	private void Awake() {
+		if (Instance == null) {
+			Instance = this;
+		} else if (Instance != this) {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start() {
+		EventsManager.Instance.gameStateChanges.AddListener(OnGameStateChanges);
+	}
+
+    void Update() {
+        
+    }
+
+	//Called when the GameState changes
+	private void OnGameStateChanges() {
+		switch (GameStatesManager.Instance.gameState) {
+			case (GameStatesManager.AvailableGameStates.Menu):
+				break;
+			case (GameStatesManager.AvailableGameStates.Starting):
+				SpawnHouse();
+				break;
+			case (GameStatesManager.AvailableGameStates.Playing):
+				break;
+			case (GameStatesManager.AvailableGameStates.Pausing):
+				break;
+			case (GameStatesManager.AvailableGameStates.Ending):
+				break;
+		}
+	}
+
+	private void SpawnHouse() {
+		GameObject house = Instantiate(availableHouses[Random.Range(0, availableHouses.Count - 1)], Vector3.zero, Quaternion.identity);
+		GameObject[] spawnList = GameObject.FindGameObjectsWithTag("SpawnPoint");
+		for (int i = 0, max = PlayerListManager.Instance.listOfPlayers.Count; i < max; i++) {
+			PlayerListManager.Instance.listOfPlayers[i].avatar.transform.position = spawnList[i].transform.position;
+		}
+	}
+}
